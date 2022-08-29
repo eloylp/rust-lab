85d508a43c946aa956c80dd682ee9b08b65da403
eloylp <eloylp@protonmail.com>
2022-08-29 19:43:14 +0200

    Add negative shift check in caesar

--- caesar/src/caesar.rs
+++ caesar/src/caesar.rs
@@ -1,11 +1,32 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 
 pub enum Mode {
     Encrypt,
     Decrypt,
 }
 
-pub fn caesar(input: &str, shift: i32, dir: Mode) -> String {
+#[derive(Debug, PartialEq)]
+pub struct NegativeShiftError;
+
+impl Display for NegativeShiftError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+impl Error for NegativeShiftError {
+    fn description(&self) -> &str {
+        "the shift parameter must be positive."
+    }
+}
+
+pub fn caesar(input: &str, shift: i32, dir: Mode) -> Result<String, NegativeShiftError> {
+    if shift.is_negative() {
+        return Err(NegativeShiftError);
+    }
+
     let alphabet_pos = [
         'a', 'b', 'c', 'd', 'e',
         'f', 'g', 'h', 'i', 'j',
@@ -40,7 +61,7 @@ pub fn caesar(input: &str, shift: i32, dir: Mode) -> String {
             None => result.push_str(ic.to_string().as_str())
         }
     }
-    return result;
+    Ok(result)
 }
 
 fn calc_index_forward(letter_index: &usize, shift: i32) -> usize {
@@ -61,96 +82,102 @@ fn calc_index_backward(letter_index: &usize, shift: i32) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use crate::caesar::{caesar, Mode};
+    use crate::caesar::{caesar, Mode, NegativeShiftError};
 
     #[test]
     fn it_encrypts_basic_string() {
-        let result = caesar("ABC", 1, Mode::Encrypt);
+        let result = caesar("ABC", 1, Mode::Encrypt).unwrap();
         assert_eq!("BCD", result);
     }
 
     #[test]
     fn it_decrypts_basic_string() {
-        let result = caesar("BCD", 1, Mode::Decrypt);
+        let result = caesar("BCD", 1, Mode::Decrypt).unwrap();
         assert_eq!("ABC", result);
     }
 
     #[test]
     fn it_ignores_but_keeps_non_alphabet_characters() {
-        let result = caesar("(ABC)D", 1, Mode::Encrypt);
+        let result = caesar("(ABC)D", 1, Mode::Encrypt).unwrap();
         assert_eq!("(BCD)E", result);
     }
 
     #[test]
     fn it_respects_spaces() {
-        let result = caesar("A B C", 1, Mode::Encrypt);
+        let result = caesar("A B C", 1, Mode::Encrypt).unwrap();
         assert_eq!("B C D", result);
     }
 
     #[test]
     fn it_respects_multiline() {
-        let result = caesar("A \n B \n C", 1, Mode::Encrypt);
+        let result = caesar("A \n B \n C", 1, Mode::Encrypt).unwrap();
         assert_eq!("B \n C \n D", result);
     }
 
     #[test]
     fn it_respects_capitalization() {
-        let result = caesar("ABC", 1, Mode::Encrypt);
+        let result = caesar("ABC", 1, Mode::Encrypt).unwrap();
         assert_eq!("BCD", result);
 
-        let result = caesar("abc", 1, Mode::Encrypt);
+        let result = caesar("abc", 1, Mode::Encrypt).unwrap();
         assert_eq!("bcd", result);
     }
 
     #[test]
     fn it_ignores_but_keeps_utf8_chars() {
-        let result = caesar("ЗaЗ", 1, Mode::Encrypt);
+        let result = caesar("ЗaЗ", 1, Mode::Encrypt).unwrap();
         assert_eq!("ЗbЗ", result)
     }
 
     #[test]
     fn it_handles_last_alpha_pos_encrypt() {
-        let result = caesar("ABC", 26, Mode::Encrypt);
+        let result = caesar("ABC", 26, Mode::Encrypt).unwrap();
         assert_eq!("ABC", result);
     }
 
     #[test]
     fn it_handles_last_alpha_pos_decrypt() {
-        let result = caesar("ABC", 26, Mode::Decrypt);
+        let result = caesar("ABC", 26, Mode::Decrypt).unwrap();
         assert_eq!("ABC", result);
     }
 
     #[test]
     // Letters close to the end and displacement exceeds last alpha.
     fn it_handles_relative_upper_overflow() {
-        let result = caesar("XY", 3, Mode::Encrypt);
+        let result = caesar("XY", 3, Mode::Encrypt).unwrap();
         assert_eq!("AB", result);
     }
 
     #[test]
     // Letters close to the end and displacement exceeds last alpha.
     fn it_handles_relative_lower_overflow() {
-        let result = caesar("BC", 3, Mode::Decrypt);
+        let result = caesar("BC", 3, Mode::Decrypt).unwrap();
         assert_eq!("YZ", result);
     }
 
     #[test]
     // Two times the alphabet + 2 (forward).
     fn it_handles_upper_bound_overflow() {
-        let result = caesar("ABC", 54, Mode::Encrypt);
+        let result = caesar("ABC", 54, Mode::Encrypt).unwrap();
         assert_eq!("CDE", result);
     }
 
     #[test]
     // Two times the alphabet + 3 (backward).
     fn it_handles_lower_bound_overflow() {
-        let result = caesar("ABC", 55, Mode::Decrypt);
+        let result = caesar("ABC", 55, Mode::Decrypt).unwrap();
         assert_eq!("XYZ", result);
     }
 
     #[test]
     fn it_returns_same_on_no_shift() {
-        let result = caesar("ABC", 0, Mode::Encrypt);
+        let result = caesar("ABC", 0, Mode::Encrypt).unwrap();
         assert_eq!("ABC", result);
+    }
+
+    #[test]
+    fn it_returns_error_on_negative_shift() {
+        let result = caesar("ABC", -1, Mode::Encrypt).unwrap_err();
+        assert_eq!(NegativeShiftError, result);
     }
 }
